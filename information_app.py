fc10dc26dfc58a4bfdfd4fa75ffcc4dd0cd8dd66
jisu0427 <jeongjisu980427@gmail.com>
2022-05-02 10:07:15 +0900

    information create

--- information_app.py
+++ information_app.py
@@ -9,6 +9,6 @@ import random
 
 def run_information_app():
     # 1. 자료 출처
+    st.markdown('자료 출처 : https://github.com/allisonhorst/palmerpenguins')
     # 2. 내 깃허브 주소
-    # 3. 
-    pass
+    st.markdown('My Github : https://github.com/jisu0427/My_Project')
